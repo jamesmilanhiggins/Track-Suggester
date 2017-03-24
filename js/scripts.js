f487fe6d1cc14eeca47fc4576bdb76be70ecfbda
jbiggins23 <jameshiggins@Jamess-MacBook-Pro.local>
2017-03-24 11:30:24 -0700

    add yourName question and added output to answer and test

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,15 @@
 $(document).ready(function() {
   $("form#developer").submit(function(event) {
+    var yourNameInput = $("input#yourName").val();
     var location = $("input:radio[name=location]:checked").val();
     // var location = $("select#location").val();
     var typeOfDev = $("select#typeOfDev").val();
     var company = $("select#company").val();
     var projects = $("select#projects").val();
     var vacay = $("select#vacay").val();
+
+
+    $(".yourName").text(yourNameInput);
 
     $(".tracks").hide();
 
